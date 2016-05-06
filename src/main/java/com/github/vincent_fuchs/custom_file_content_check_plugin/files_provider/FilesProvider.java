eaa92c9e7d4b5206b09a21a7d27aea59d86e38c3
vincent-fuchs <vincent.fuchs@gmail.com>
2016-05-06 09:28:00 +0530

    removing "author comment"

--- src/main/java/com/github/vincent_fuchs/custom_file_content_check_plugin/files_provider/FilesProvider.java
+++ src/main/java/com/github/vincent_fuchs/custom_file_content_check_plugin/files_provider/FilesProvider.java
@@ -1,7 +1,4 @@
 package com.github.vincent_fuchs.custom_file_content_check_plugin.files_provider;
 
-/**
- * Created by vfuchs070114 on 5/5/2016.
- */
 public interface FilesProvider {
 }
--- src/main/java/com/github/vincent_fuchs/custom_file_content_check_plugin/rules_to_apply/RulesToApply.java
+++ src/main/java/com/github/vincent_fuchs/custom_file_content_check_plugin/rules_to_apply/RulesToApply.java
@@ -1,7 +1,4 @@
 package com.github.vincent_fuchs.custom_file_content_check_plugin.rules_to_apply;
 
-/**
- * Created by vfuchs070114 on 5/5/2016.
- */
 public interface RulesToApply {
 }
